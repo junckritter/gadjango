c4dab6736c9fa5a9a08415e37f5b14234c9b2ca6
Tomas Sirny <sirny@synopsi.tv>
2012-10-28 20:23:24 +0100

    Setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GADjango',
+      version='0.0.1',
+      description='Simple Performance data sender to Google Analytics',
+      author='Tomas Sirny',
+      author_email='tsirny@gmail.com',
+      url='https://github.com/junckritter/gadjango',
+      packages=['gadjango'],
+     )
