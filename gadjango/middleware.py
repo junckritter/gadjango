8fe11297c268e056ac4abd846a87f8096a5a0391
Tomas Sirny <sirny@synopsi.tv>
2012-10-31 08:39:46 +0100

    Fix.

--- gadjango/middleware.py
+++ gadjango/middleware.py
@@ -9,12 +9,12 @@ class TimingMiddleware(object):
     """
 
     def process_request(self, request):
-        self.request._start_total_time = time.time()
+        request._start_total_time = time.time()
         return None
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        self.request._start_view_time = time.time()
-        self.request._view_name = view_func.__name__
+        request._start_view_time = time.time()
+        request._view_name = view_func.__name__
         return None
 
 
@@ -24,14 +24,14 @@ class TimingMiddleware(object):
             {
                 'category': 'Django View',
                 'label': 'view_time',
-                'time': int((now - self.request._start_view_time) * 1000),
-                'view': self.request._view_name
+                'time': int((now - request._start_view_time) * 1000),
+                'view': request._view_name
             },
             {
                 'category': 'Django View',
                 'label': 'total_time',
-                'time': int((now - self.request._start_total_time) * 1000),
-                'view': self.request._view_name
+                'time': int((now - request._start_total_time) * 1000),
+                'view': request._view_name
             }
         ]
         response.context_data.update({'ga_stats': stats})
