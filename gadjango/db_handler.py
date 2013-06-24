71cfa380e8132e81d2db26ebc0c05504d0cfeb30
Pavel Koci <koci@synopsi.tv>
2013-06-24 11:09:54 +0200

    Database statistics.

--- gadjango/db_handler.py
+++ gadjango/db_handler.py
@@ -1,36 +1,31 @@
 """
 Database handlers for gadjango.
 """
-from time import time
+from datetime import datetime
 
 from django.db import connections
 from django.db.backends.util import CursorWrapper, CursorDebugWrapper
 
 
 def execute(self, sql, params=()):
-    start = time()
+    start = datetime.now()
     try:
         return self.cursor.execute(sql, params)
     finally:
-        stop = time()
-        duration = stop - start
-        sql = self.db.ops.last_executed_query(self.cursor, sql, params)
         self.db.queries.append({
             'sql': sql,
-            'time': int(float("%.6f" % duration) * 1000000),
+            'time': _ms(datetime.now() - start),
         })
 
 
 def executemany(self, sql, param_list):
-    start = time()
+    start = datetime.now()
     try:
         return self.cursor.executemany(sql, param_list)
     finally:
-        stop = time()
-        duration = stop - start
         self.db.queries.append({
             'sql': '%s times: %s' % (len(param_list), sql),
-            'time': int(float("%.6f" % duration) * 1000000),
+            'time': _ms(datetime.now() - start),
         })
 
 
@@ -45,12 +40,22 @@ def get_db_stats():
     """
     Get queries from all connections.
     """
-    all_queries = []
+    total_time = 0.0
+    queries = []
     for c in connections.all():
         for q in c.queries:
-            all_queries.append({
-                'time': q['time'],
-                'sql': '%s:%s' % (c.alias, q['sql'])
+            queries.append({
+                'time': int(float('%.3f' % q['time'])),
+                'sql': '%s: %s' % (c.alias, q['sql'][:64].replace('\n', ' ').replace("'", "\\'"))
             })
+            total_time += q['time']
 
-    return all_queries
+    return {
+        'queries': queries,
+        'total_time': int(float('%.3f' % total_time)),
+        'query_count': len(queries)
+    }
+
+
+def _ms(time_delta):
+    return int((time_delta.days * 24 * 60 * 60 + time_delta.seconds) * 1000 + time_delta.microseconds / 1000.0)
--- gadjango/middleware.py
+++ gadjango/middleware.py
@@ -5,6 +5,9 @@ import time
 
 from db_handler import get_db_stats
 
+from django.conf import settings
+from django.template.loader import render_to_string
+
 
 class TimingMiddleware(object):
     """
@@ -19,7 +22,15 @@ class TimingMiddleware(object):
         self._view_name = view_func.__name__
         return None
 
-    def process_template_response(self, request, response):
+    def process_response(self, request, response):
+        # append GA at the bottom on the page
+        response.content = response.content.replace('</body>', '%s</body>' % str(
+            render_to_string('gadjango/timing.html', {'ga_stats': self._get_stats()})
+        ))
+
+        return response
+
+    def _get_stats(self):
         now = time.time()
         stats = [
             {
@@ -36,15 +47,32 @@ class TimingMiddleware(object):
             }
         ]
 
-        db_stats = get_db_stats()
-        if db_stats:
-            for item in db_stats:
+        if getattr(settings, 'GADJANGO_DB_STATS', False):
+            # get DB stats only if GADJANGO_DB_STATS is True
+            db_stats = get_db_stats()
+            if db_stats:
                 stats.append({
                     'category': 'Django DB',
-                    'label': 'sql',
-                    'time': item['time'],
-                    'view': item['sql']
+                    'label': 'total_time',
+                    'time': db_stats['total_time'],
+                    'view': self._view_name
+                })
+                stats.append({
+                    'category': 'Django DB',
+                    'label': 'query_count',
+                    'time': db_stats['query_count'],
+                    'view': self._view_name
                 })
 
-        response.context_data.update({'ga_stats': stats})
-        return response
+                if getattr(settings, 'GADJANGO_DB_QUERIES', False):
+                    # get DB queries if GADJANGO_DB_QUERIES is True
+                    # don't use this on live environment!
+                    for item in db_stats['queries']:
+                        stats.append({
+                            'category': 'Django DB',
+                            'label': 'sql',
+                            'time': item['time'],
+                            'view': item['sql']
+                        })
+
+        return stats
